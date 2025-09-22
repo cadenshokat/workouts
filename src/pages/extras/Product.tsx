1c9bc6db3d9e3c9ce69a8130e0ce5ae00f50b449
Caden Shokat <cadenshokat@gmail.com>
2025-09-22 11:20:51 -0400

    add levers air table for product section

--- src/pages/extras/Product.tsx
+++ src/pages/extras/Product.tsx
@@ -3,14 +3,26 @@ import { ConfigurableSection } from "@/components/ConfigurableSection";
 export const Product = () => {
   return (
     <div className="p-6">
-      <h1 className="text-lg font-bold">Product</h1>
-      <iframe 
-        src="https://airtable.com/embed/appwQNBnpJKVEWDhQ/shrGNNslwZHR4IbEj?viewControls=on" 
-        width="100%"
-        height="600"
-        style={{ border: 'none' }}
-        title="Marketing Data"
-      />
+      <div>
+        <h1 className="text-lg font-bold">Product</h1>
+        <iframe 
+          src="https://airtable.com/embed/appwQNBnpJKVEWDhQ/shrGNNslwZHR4IbEj?viewControls=on" 
+          width="100%"
+          height="600"
+          style={{ border: 'none' }}
+          title="Product Data"
+        />
+      </div>
+      <div>
+        <h1 className="text-lg font-bold">Product Levers</h1>
+        <iframe 
+          src="https://airtable.com/embed/appwQNBnpJKVEWDhQ/shrDhS03gMF3fP4U8?viewControls=on" 
+          width="100%" 
+          height="600" 
+          style={{ background: "transparent", border: "none"}}
+          title="Product Levers Data"
+        />
+      </div>
     </div>
   );
 };
